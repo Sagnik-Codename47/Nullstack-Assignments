b645074d7e80162ce6b0bb8eb64ec3cab67f9315
Sagnik-Codename47 <79573876+Sagnik-Codename47@users.noreply.github.com>
2021-05-14 15:42:38 +0530

    Update Program 2.py

--- Hackathon 1/Program 2.py
+++ Hackathon 1/Program 2.py
@@ -1,7 +1,7 @@
+from collections import Counter
 n=int(input())
 for i in range(n):
-    marks = map(int,input().split())
-    for i in marks:
-    	if i>37 and i%5>2:
-    		i+=5-(i%5)
-    	print(i)
+	socks, pairs = Counter(map(int,input().strip().split())), 0
+	for s in socks: 
+		pairs += socks[s]//2
+	print(pairs)
